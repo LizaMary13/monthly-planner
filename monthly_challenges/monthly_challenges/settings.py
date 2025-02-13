72011ec86077b7bf656961b312689b9b5f7cc19b
Liza Mathews <lizliy1316@gmail.com>
2025-02-13 13:30:26 +0530

    Deploying using K8s

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w**kt57tojm!lx24)ad=g%cxavi1@f_#xyr!ekr8540jp%!j%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
